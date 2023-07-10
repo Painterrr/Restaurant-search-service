05e086ce707ea41e5aeb6920ff07e4e8dffdd02d
KunSung <tttzkq9336@gmail.com>
2023-07-10 23:25:15 +0900

    데이터 생성하기 with JPA DB에 게시글 저장하기 add articleRepository

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,13 +1,18 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    @Autowired // spring boot가 미리 생성해놓은 객체를 자동 연결
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -17,6 +22,15 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+
+        // 1. DTO를 Entity로 변환
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        // 2. Repository에게 Enntity를 DB 안에 저장하게 함
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+
         return "";
     }
 }
