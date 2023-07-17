e08bbbd0322ef5e05a79ec1cd7c73bb566c23bcb
KunSung <tttzkq9336@gmail.com>
2023-07-17 23:19:32 +0900

    add data.sql
    error: Caused by: org.springframework.jdbc.datasource.init.ScriptStatementFailedException: Failed to execute SQL script statement #1 of URL [file:/C:/Users/tttzk/Desktop/firstproject/firstproject/out/production/resources/data.sql]: INSERT INTO article(id, title, content) values (1, 'dummy1', '1111'); nested exception is org.h2.jdbc.JdbcSQLSyntaxErrorException: Table "ARTICLE" not found (this database is empty); SQL statement:
           INSERT INTO article(id, title, content) values (1, 'dummy1', '1111') [42104-214]
    
    solution: ?

---
+++ firstproject/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(id, title, content) values (1, 'dummy1', '1111');
+INSERT INTO article(id, title, content) values (2, 'dummy2', '2222');
+INSERT INTO article(id, title, content) values (3, 'dummy3', '3333');
