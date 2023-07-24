e78a4b467504f3c6ab4d2f2e932e9b8bdefc53cc
KunSung <tttzkq9336@gmail.com>
2023-07-24 11:36:32 +0900

    error: 1. {"id":4,"title":null,"content":null} 2. binding parameter [1] as [VARCHAR] - [null], binding parameter [2] as [VARCHAR] - [null]
    solution:
    1. add @RequestBody annotation in create Method in ArticleApiController
    2. ? (in ArticleForm, add @NoArgsConstructor. but the problem doesn't solve)
    2-1. add @Setter in ArticleForm. I guess I missed the annotation.

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -3,11 +3,13 @@ package com.example.firstproject.dto;
 import com.example.firstproject.entity.Article;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Setter
 public class ArticleForm {
     // controller에서 form 데이터를 받을 dto
     // form에서 받을 데이터 명과 일치
