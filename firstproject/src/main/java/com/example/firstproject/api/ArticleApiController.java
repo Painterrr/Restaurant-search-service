e271ad846c619e61655b63941e0a0f48debca248
KunSung <tttzkq9336@gmail.com>
2023-07-24 13:02:52 +0900

    add PATCH method

--- firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,12 +3,16 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 public class ArticleApiController {
     @Autowired // DI
     private ArticleRepository articleRepository;
@@ -31,9 +35,28 @@ public class ArticleApiController {
         return articleRepository.save(article);
     }
 
-
     // PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
 
+        // 1. create updating Entity
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+
+        // 2. select target Entity
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 3. deal Wrong Request(there is no target or different id)
+        if (target == null || id != article.getId()) {
+            // 400. Response "BAD Request"
+            log.info("BAD Request! id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        // 4. update and response
+        Article updated = articleRepository.save(article);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
 
     // DELETE
 
